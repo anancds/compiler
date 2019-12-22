54d5a67bc773b93d0df1e9f65a4c591573c04a0f
anancds <anancds@163.com>
2019-12-22 21:06:34 +0800

    finished cmake step 2

--- tutorial.cpp
+++ tutorial.cpp
@@ -1,23 +1,33 @@
 // A simple program that computes the square root of a number
 #include <cstdio>
 #include <cstdlib>
-//#include <math.h>
+#include <cmath>
 #include "TutorialConfig.h"
 #include "mysqrt.h"
 #include <string>
 #include <iostream>
 
-int main (int argc, char *argv[])
-{
+#ifdef USE_MYMATH
+#include "mysqrt.h"
+#endif
+
+int main(int argc, char *argv[]) {
     if (argc < 2) {
         // report version
         std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
                   << Tutorial_VERSION_MINOR << std::endl;
         std::cout << "Usage: " << argv[0] << " number" << std::endl;
 
-        const double inputValue = std::stod(argv[1]);
 
         return 1;
     }
+
+    const double inputValue = std::stod(argv[1]);
+#ifdef USE_MYMATH
+    const double outputValue = mysqrt(inputValue);
+        std::cout << outputValue << std::endl;
+#else
+    const double outputValue = sqrt(inputValue);
+#endif
 }
 
